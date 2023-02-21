e48b1cca89bc3419c1ed9192d11ecf9486868a48
JeffreyPope <agyemangjeff13@gmail.com>
2023-02-21 15:54:44 -0800

    extra tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,46 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * main - check for multiples of 3 and 5
+ *
+ * Return: 0 always
  */
-void print_times_table(int n)
+int main(void)
 {
-	int i, j, k;
+	int x = 1024, y, sum = 0;
 
-	if (n >= 0 && n <= 15)
+	for (y = 0; y < x; y++)
 	{
-		for (i = 0; i <= n; i++)
+		if ((y % 3 == 0) || (y % 5 == 0))
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			sum = sum + y;
 		}
 	}
+	printf("%d\n", sum);
+	return (0);
 }
